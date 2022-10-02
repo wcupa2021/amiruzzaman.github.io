9d31c942fce9d3e15ddba3224a642127e20bdf13
Md Amiruzzaman <mamiruzz@kent.edu>
2022-10-01 21:35:31 -0400

    Adding some utility work

---
+++ student/coin/data/mydata.js
@@ -0,0 +1,779 @@
+[
+  {
+    "Country": "Afganistan",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Jawid"
+  },
+  {
+    "Country": "Albania",
+    "Coin": "",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Sarah R."
+  },
+  {
+    "Country": "Algeria",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Henna"
+  },
+  {
+    "Country": "Argentina",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Karina, Trade"
+  },
+  {
+    "Country": "Australia",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Bria, Fikir, Gary"
+  },
+  {
+    "Country": "Austria",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "Old",
+    "Donor": "Amir, Antique"
+  },
+  {
+    "Country": "Bahamas",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Cheryl, Sarah, Glenda, Antique"
+  },
+  {
+    "Country": "Bangladesh",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "Amir, Shamid, Khaled, Anisur, Stef"
+  },
+  {
+    "Country": "Belgium",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "Old",
+    "Donor": "Cheryl, Trade, Antique"
+  },
+  {
+    "Country": "Bermuda",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Kyle Evans"
+  },
+  {
+    "Country": "Bhutan",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Trade"
+  },
+  {
+    "Country": "Bolivia",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Antique"
+  },
+  {
+    "Country": "Brazil",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Braga, Antique, Aryn"
+  },
+  {
+    "Country": "Bulgeria",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Yulian"
+  },
+  {
+    "Country": "Burkina Faso",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Sofie"
+  },
+  {
+    "Country": "Cameroon",
+    "Coin": "",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Bate"
+  },
+  {
+    "Country": "Canada",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "Amir, Cheryl, Josh, Kyoko, Antique"
+  },
+  {
+    "Country": "Chile",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "Antique, Aryn"
+  },
+  {
+    "Country": "China",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Rita, Amir, Phoebe, Le, Bill, Dr Saunders"
+  },
+  {
+    "Country": "Colombia",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "Martin, Antique"
+  },
+  {
+    "Country": "Congo",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Cedrik"
+  },
+  {
+    "Country": "Congo, The Democratic Republic of The",
+    "Coin": "",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Cedrik"
+  },
+  {
+    "Country": "Costa Rica",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Mariana"
+  },
+  {
+    "Country": "Croatia",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Dr Saunders"
+  },
+  {
+    "Country": "Cyprus",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Trade"
+  },
+  {
+    "Country": "Czech Republic",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Nadia"
+  },
+  {
+    "Country": "Dominican Republic",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Bria, Bobi"
+  },
+  {
+    "Country": "Ecuador",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "JJ, David, Antique, Craig Z"
+  },
+  {
+    "Country": "Egypt",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Dr Saunders, Vahid"
+  },
+  {
+    "Country": "El Savador",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Trade"
+  },
+  {
+    "Country": "Ethiopia",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Trade"
+  },
+  {
+    "Country": "Fiji",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Kyoko"
+  },
+  {
+    "Country": "Finland",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Trade"
+  },
+  {
+    "Country": "France",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "Old",
+    "Donor": "Amir, Antique"
+  },
+  {
+    "Country": "Germany",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "Old",
+    "Donor": "Subrata, Amir, Antique, Craig Z"
+  },
+  {
+    "Country": "Ghana",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Cedrik"
+  },
+  {
+    "Country": "Greece",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "Old",
+    "Donor": "Mike Radak, Antique, Aryn"
+  },
+  {
+    "Country": "Guatemala",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "JJ, Java"
+  },
+  {
+    "Country": "Haiti",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "JJ"
+  },
+  {
+    "Country": "Hong Kong",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Cheryl, Oscar"
+  },
+  {
+    "Country": "Hungary",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Jolton, Cheryl, Alex-Tiff"
+  },
+  {
+    "Country": "India",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "Trade, Subrata, David, Surekha, Kedar"
+  },
+  {
+    "Country": "Indonesia",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Bayu"
+  },
+  {
+    "Country": "Iran, Islamic Republic of",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Vahid, Bahar, Shadi"
+  },
+  {
+    "Country": "Iraq",
+    "Coin": "",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "OIA, N'war, Farah"
+  },
+  {
+    "Country": "Ireland",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Amir"
+  },
+  {
+    "Country": "Isreal",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Cheryl"
+  },
+  {
+    "Country": "Italy",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "Sarah, Tessa, Antique"
+  },
+  {
+    "Country": "Jamaica",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "JJ, Antique"
+  },
+  {
+    "Country": "Japan",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Kyoko, Trade, Sarah"
+  },
+  {
+    "Country": "Jordan",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Mohammad, Kareem, Fadi"
+  },
+  {
+    "Country": "Jugoslavia",
+    "Coin": "Coins",
+    "PaperBill": "Old",
+    "Type": "",
+    "Donor": "Trade, Antique"
+  },
+  {
+    "Country": "Kenya",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Cheryl, Josiah, Fred N"
+  },
+  {
+    "Country": "Korea, Republic of",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Amir, Busem, Tori, Cheryl"
+  },
+  {
+    "Country": "Kuwait",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "David"
+  },
+  {
+    "Country": "Libyan Arab Jamahiriya",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "OIA"
+  },
+  {
+    "Country": "Lithunia",
+    "Coin": "",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Bate"
+  },
+  {
+    "Country": "Macedonia, The Former Yugoslav Republic of",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Dr Saunders"
+  },
+  {
+    "Country": "Malaysia",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Joey"
+  },
+  {
+    "Country": "Mexico",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Mariana, JJ, Oscar, Craig Z"
+  },
+  {
+    "Country": "Mali",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Abudu"
+  },
+  {
+    "Country": "Mongolia",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Altai"
+  },
+  {
+    "Country": "Morocco",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "Loubna, Antique"
+  },
+  {
+    "Country": "Nepal",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Harish, Jagat, Deepshikha"
+  },
+  {
+    "Country": "Netherlands",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "Old",
+    "Donor": "Amir, Kyoko, Antique"
+  },
+  {
+    "Country": "New Zealand",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Kyoko"
+  },
+  {
+    "Country": "Nicaragua",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "JJ"
+  },
+  {
+    "Country": "Nigeria",
+    "Coin": "",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Cedrik"
+  },
+  {
+    "Country": "Norway",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Trade"
+  },
+  {
+    "Country": "Oman",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Amal"
+  },
+  {
+    "Country": "Pakistan",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "Imran"
+  },
+  {
+    "Country": "Panama",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Amir, JJ, Josh, Antique"
+  },
+  {
+    "Country": "Papua New Guinea",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Antique, Craig Z"
+  },
+  {
+    "Country": "Paraguay",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "Old",
+    "Donor": "Antique"
+  },
+  {
+    "Country": "Peru",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "JJ, Antique"
+  },
+  {
+    "Country": "Philippines",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Eilona"
+  },
+  {
+    "Country": "Poland",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Weronica, Java, Chris O"
+  },
+  {
+    "Country": "Portugal",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "Trade, Antique"
+  },
+  {
+    "Country": "Qatar",
+    "Coin": "",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Joey"
+  },
+  {
+    "Country": "Romania",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Amanda"
+  },
+  {
+    "Country": "Russian Federation",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Yana, Marina, Vasiliy, Maxim, Nadia"
+  },
+  {
+    "Country": "Saudi Arabia",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Talal, OIA, Ahmed, Nada, Haifa"
+  },
+  {
+    "Country": "Singapore",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Bayu, Phu"
+  },
+  {
+    "Country": "Slovakia",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Martin"
+  },
+  {
+    "Country": "Solvenija",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Martin"
+  },
+  {
+    "Country": "South Africa",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Shelley, Cheryl"
+  },
+  {
+    "Country": "Spain",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Cedrik"
+  },
+  {
+    "Country": "Sri Lanka",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Randy"
+  },
+  {
+    "Country": "Suriname",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "Old",
+    "Donor": "Antique"
+  },
+  {
+    "Country": "Sweden",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Trade, Antique"
+  },
+  {
+    "Country": "Switzerland",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Tori, Dr Saunders, Josh, Antique"
+  },
+  {
+    "Country": "Syrian Arab Republic",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Kareem"
+  },
+  {
+    "Country": "Taiwan, Province of China",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Stefanie, Tiffani, Amir"
+  },
+  {
+    "Country": "Tanzania, United Republic of",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Kyoko"
+  },
+  {
+    "Country": "Thailand",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "Pom, Bate, Antique, Parn"
+  },
+  {
+    "Country": "Trinidad & Tobago",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "Trade, Antique"
+  },
+  {
+    "Country": "Tunasia",
+    "Coin": "",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "Antique"
+  },
+  {
+    "Country": "Turkey",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "Busem, Bulut, Dr Saunders, Antique, Riza, Ilker"
+  },
+  {
+    "Country": "Turkmenistan",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Mayaa"
+  },
+  {
+    "Country": "Uganda",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Josiah, Lori"
+  },
+  {
+    "Country": "Ukraine",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Trade, Galaka"
+  },
+  {
+    "Country": "United Arab Emirates",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "David"
+  },
+  {
+    "Country": "United Kingdom",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "Old",
+    "Donor": "Amir, Sarah, Dr Saunders, Kyoko, Kyle, Antique, Craig Z"
+  },
+  {
+    "Country": "United States",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Stefanie, Seth, Josh, Ted"
+  },
+  {
+    "Country": "Uruguay",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "Old",
+    "Donor": "Antique"
+  },
+  {
+    "Country": "Uzbekistan",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": "Trade"
+  },
+  {
+    "Country": "Venezuela",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Cheryl, Bate"
+  },
+  {
+    "Country": "Viet Nam",
+    "Coin": "Coins",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Cheryl, Amir"
+  },
+  {
+    "Country": "West Africa",
+    "Coin": "Coins",
+    "PaperBill": "",
+    "Type": "",
+    "Donor": ""
+  },
+  {
+    "Country": "Zimbabwe",
+    "Coin": "",
+    "PaperBill": "Paper Bill",
+    "Type": "",
+    "Donor": "Bate"
+  },
+  {
+    "Country": "",
+    "Coin": 101,
+    "PaperBill": 54,
+    "Type": 25,
+    "Donor": ""
+  }
+]
